5af92bcd119d19faa51ec23991886596cf0d01c6
Daniela Duque <danduquegar@unal.edu.co>
2019-09-27 14:56:05 -0500

    version 3

--- menuinicio/src/App.js
+++ menuinicio/src/App.js
@@ -4,42 +4,47 @@ import './App.css';
 import foto1 from './Images/imagen1.jpg';
 import foto2 from './Images/imagen2.jpg';
 import foto3 from './Images/imagen3.jpg';
+import pasear from './Images/pasear.jpg';
+import salto from './Images/salto.jpg';
+import veterinaria from './Images/veterinaria.jpg';
+import guarderia from './Images/guarderia.jpg';
 
 class App extends Component{
   render(){
   return (
-    <div className="App">
+    <div className="App color-ligth">
       <header>
       <Navegation ></Navegation>
       </header>
      
       <body>
+        <div className="color-ligth">
       <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
             <ol className="carousel-indicators">
               <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
               <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
               <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
             </ol>
-            <div className="carousel-inner">
+            <div className="carousel-inner text-align">
               <div className="carousel-item active"  data-interval="1000">
                  <img src={foto1} className="d-block w-100" alt="logo"></img>
                    <div className="carousel-caption d-none d-md-block">
-                     <h5  >First slide label</h5>
-                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                     <h5 className="text-uppercase">Un mundo de posibilidades</h5>
+                     <p>Para el cuidado de tu mascota.</p>
                    </div>
                </div>
-              <div className="carousel-item" data-interval="1000">
+              <div className="carousel-item" >
                 <img src={foto2} class="d-block w-100" alt="logo"></img>
                 <div className="carousel-caption d-none d-md-block">
-                  <h5 clasName = "test-format">Second slide label</h5>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <h5 className="text-uppercase">La única aplicación que te ofrece</h5>
+                  <p>El servicio de saltos.</p>
                 </div>
               </div>
-              <div className="carousel-item" data-interval="1000">
+              <div className="carousel-item" >
                 <img src={foto3} class="d-block w-100" alt="logo"></img>
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>Third slide label</h5>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div className="carousel-caption d-none d-md-block ">
+                  <h5 className="text-uppercase">Lo que quieres para tu mascota </h5>
+                  <p>En un solo lugar.</p>
                 </div>
               </div>
             </div>
@@ -52,6 +57,8 @@ class App extends Component{
               <span className="sr-only">Next</span>
               </a>
           </div>
+
+          {/*
           <div>
             <ul className="nav flex-column">
               <li className="nav-item">
@@ -87,7 +94,42 @@ class App extends Component{
            
             </ul>
           </div>
-      </body>     
+          */}
+
+          <h1 className = "text-align padding-section">Nuestros servicios</h1>
+          
+          <div class="container marketing text-align padding-card">
+
+          <div className="row">
+            <div className="col-lg-3">
+              <img src={pasear} alt="Generic placeholder image" width="140" height="140"/>
+              <h2>Paseos</h2>
+              <p>Accede a un catálogo de paseadores para tu mascota.</p>
+              <p><a class="btn btn-outline-dark" href="#" role="button">Más información »</a></p>
+            </div>
+            <div className="col-lg-3">
+              <img src={veterinaria} alt="Generic placeholder image" width="140" height="140"/>
+              <h2>Veterinaria</h2>
+              <p>Encuentra la veterinaria de tu preferencia.</p>
+              <p><a className="btn btn-outline-dark" href="#" role="button">Más información »</a></p>
+            </div>
+            <div className="col-lg-3">
+              <img  src={salto} alt="Generic placeholder image" width="140" height="140"/>
+              <h2>Saltos</h2>
+              <p>Las mejores mascotas para tus cruces.</p>
+              <p><a className="btn btn-outline-dark" href="#" role="button">Más información »</a></p>  
+            </div>
+            <div className="col-lg-3">
+              <img  src={guarderia} alt="Generic placeholder image" width="140" height="140"/>
+              <h2>Guardería</h2>
+              <p>El mejor cuidado para tu mascota cuando no puedes estar con ella.</p>
+              <p><a className="btn btn-outline-dark" href="#" role="button">Más información »</a></p>  
+            </div>
+          </div>
+        </div>
+        </div>
+      </body>  
+         
       <footer>
         
         <Navegation>
--- menuinicio/src/components/Navegation.js
+++ menuinicio/src/components/Navegation.js
@@ -12,9 +12,9 @@ class Navegation extends Component{
                 <h1 className= "text-light  text-font font-italic text-uppercase ">
                     doky
                 </h1>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search"></input>
-                    <button className=" btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+                <form className="form-inline my-2 my-lg-0 text-font ">
+                    <input className="form-control mr-sm-2 " type="search" placeholder="Buscar" aria-label="Search"></input>
+                    <button className=" btn btn btn-outline-light my-2 my-sm-0" type="submit">Buscar</button>
                  </form>
 
                  <img src={logo} className="logo-size" alt="logo" ></img>
