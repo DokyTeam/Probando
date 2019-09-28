834293c75be738d4459d2c278decaad2d0dbdad0
Daniela Duque <danduquegar@unal.edu.co>
2019-09-27 20:39:03 -0500

    agregar landing page

--- doky/src/layout/homepage/homepage.js
+++ doky/src/layout/homepage/homepage.js
@@ -3,9 +3,31 @@ import React, { Component } from 'react';
 class Homepage extends Component {
     render() {
         return (
-            <h1>
-                Hola
-            </h1>
+            <div className="color-backgroud">
+      <header>
+      <Navegation ></Navegation>
+      </header>
+     
+      
+        <div className="text-align text-font ">
+          <Carrusel></Carrusel>
+          <h1 className = "text-section padding-section">Nuestros servicios</h1>
+          <Service></Service>
+
+                   
+          
+          
+        </div>
+       
+         
+      <footer>
+        
+        <Navegation>
+
+        </Navegation>
+      </footer>
+
+    </div>
         );
     }
 }
--- doky/src/layout/upperbar/upperbar.js
+++ doky/src/layout/upperbar/upperbar.js
@@ -5,19 +5,26 @@ import shop from "./images/shop.jpeg"
 class Upperbar extends Component {
     render() {
         return (
-            <nav className="navbar  color-dark">
-                
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={this.props.MainContainerhandler.bind(this,true)}>
-                <h1 className="text-light  text-font font-italic text-uppercase ">
-                    doky
-                </h1>
-                </button>
-                <form className="form-inline my-2 my-lg-0 text-font ">
-                    <input className="form-control mr-sm-2 " type="search" placeholder="Buscar" aria-label="Search"></input>
-                    <button className=" btn btn btn-outline-light my-2 my-sm-0" type="submit">Buscar</button>
-                </form>
-                <img src={shop} className="logo-size" alt="logo" ></img>
-            </nav>
+            <div className="Navegation">
+
+
+                <div class="d-flex bd-highlight color-dark align-items-center">
+                    <div class="p-2 bd-highlight"><h1 className="text-light text-font font-italic text-uppercase padding-title">
+                        doky
+                </h1></div>
+                    <div class="p-2 bd-highlight"><input className="form-control mr-sm-1 d-none d-sm-block font-weight-bolder" type="search" placeholder="Buscar" aria-label="Search"></input>
+                    </div>
+                    <div class="p-2 bd-highlight"><button className=" btn btn-outline-light my-2 my-sm-0 d-none d-sm-block font-weight-bolder" type="submit">Buscar</button>
+                    </div>
+                    <div class="ml-auto p-2 bd-highlight"><img src={shop} className="ustify-content-end logo-size" alt="logo" ></img>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
         );
     }
 }
